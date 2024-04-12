0f4a4c7ba760a61c1b1f7545c100cf366cc78fb2
Caio Oliveira <caio.oliveira07@gmail.com>
2024-04-12 00:34:14 -0300

    Home ajustada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,12 @@ import Skills from '../Componentes/Skills';
 import ContactForm from '../Componentes/ContactForm'
 
 function Home() {
-  const skillList = [
-    { name: 'HTML', level: 'Avançado' },
-    { name: 'CSS', level: 'Intermediário' },
-    { name: 'JavaScript', level: 'Básico' }
-  ]
 
   return (
     <div className="mainInfo">
       <section id='info'>
-          <PersonalInfo 
-            name="João" 
-            email="joao@example.com" 
-            phone="(00) 12345-6789" 
-          />
-          <Skills skills={skillList} />
+          <PersonalInfo/>
+          <Skills />
         </section>
         <ContactForm />
     </div>
